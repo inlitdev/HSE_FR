98fed43965d23e17cffc53aa06b35645b9a7c575
unknown <mbadox45@gmail.com>
2022-08-04 16:03:25 +0700

    update url api

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,11 +1,11 @@
 //import axios from 'axios';
 
 // ----- server -----
-// const api = 'http://192.168.1.223:3033/';
+const api = 'http://192.168.1.225:6000/';
 
 // ----- dev -----
 // const api = 'http://localhost:3021/';
-const api = 'http://localhost:3022/';
+// const api = 'http://localhost:3022/';
 
 class BaseURL { 
 	getHost(){
--- src/api/http-common.js
+++ src/api/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export default axios.create({
     // baseURL: 'http://localhost:3033/',
-    baseURL: 'http://localhost:3022/',
-    // baseURL: 'http://192.168.1.223:3033/',
+    // baseURL: 'http://localhost:3022/',
+    baseURL: 'http://192.168.1.225:6000/',
     headers: {
         'Content-type' : 'application/json',
         'Authorization' :  `Bearer ${localStorage.usertoken}`
