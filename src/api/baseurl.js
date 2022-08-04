488b001f80ea933bb0e8bc59b5ea641127a060ef
unknown <mbadox45@gmail.com>
2022-08-04 16:12:10 +0700

    back url

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,7 +1,8 @@
 //import axios from 'axios';
 
 // ----- server -----
-const api = 'http://192.168.1.225:6000/';
+// const api = 'http://192.168.1.225:6000/';
+const api = 'http://localhost:6000/';
 
 // ----- dev -----
 // const api = 'http://localhost:3021/';
