d8e57ce3d8235083aadffabf01c1cc8fa1f5dee1
unknown <mbadox45@gmail.com>
2022-08-04 16:16:15 +0700

    rollback

--- src/api/http-multipart.js
+++ src/api/http-multipart.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'http://localhost:6000/',
+    // baseURL: 'http://localhost:6000/',
     // baseURL: 'http://localhost:3022/',
-    // baseURL: 'http://192.168.1.225:6000/',
+    baseURL: 'http://192.168.1.225:6000/',
     headers: {
         'Content-type' : 'multipart/form-data',
         'Authorization' :  `Bearer ${localStorage.usertoken}`
